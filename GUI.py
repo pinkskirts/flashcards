b496a4e79b07062bee0537d0cbfea953c0689fcd
Guilherme Monteiro de Oliveira <monteiro.oliveira1@outlook.com>
2023-05-21 21:36:11 +0000

    adicionar modulo Builder do kivy

--- GUI.py
+++ GUI.py
@@ -1,7 +1,26 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+
+class MenuPrincipal(Screen):
+    pass
+
+class SessaoFlashcards(Screen):
+    pass
+
+class Configuracoes(Screen):
+    pass
+
+class GerenciadorJanelas(ScreenManager):
+    pass
+
+kv = Builder.load_file('interface.kv')
 
 class Aplicacao(App):
     def build(self):
-        return Label(text="Hello World")
+        return kv
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from GUI import Aplicacao
+
 if __name__ == '__main__':
     Aplicacao().run()
 
